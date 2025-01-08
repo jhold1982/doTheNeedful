f9dae0958367686178a64bc44d6259dae146dfba
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-01-08 13:28:03 -0600

    Initial Commit

---
+++ doTheNeedful/doTheNeedfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  doTheNeedfulApp.swift
+//  doTheNeedful
+//
+//  Created by Justin Hold on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct doTheNeedfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
