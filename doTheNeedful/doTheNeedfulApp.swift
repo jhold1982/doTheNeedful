1ef7da5f79f5ed0bb84a30f81afaf414959613d5
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-01-08 13:49:21 -0600

    Added Google Firebase backend

--- doTheNeedful/doTheNeedfulApp.swift
+++ doTheNeedful/doTheNeedfulApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct doTheNeedfulApp: App {
+	
+	init() {
+		FirebaseApp.configure()
+	}
+	
     var body: some Scene {
         WindowGroup {
             ContentView()
