6144209a4af5f2efacc29a6000f0a566cfd70cd9
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-02-02 15:21:53 -0600

    Added login form validation

--- doTheNeedful/ViewModels/LoginViewViewModel.swift
+++ doTheNeedful/ViewModels/LoginViewViewModel.swift
@@ -6,25 +6,44 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 class LoginViewViewModel: ObservableObject {
 	
 	@Published var email = ""
 	@Published var password = ""
+	@Published var errorMessage = ""
 	
 	init() {
 		
 	}
 	
 	func login() {
-		guard !email.trimmingCharacters(in: .whitespaces).isEmpty, !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+		guard validate() else {
 			return
 		}
-		
+		// Try to log in
+		Auth.auth().signIn(withEmail: email, password: password)
 		print("DEBUG: user entered \(email) & \(password)")
 	}
 	
-	func validate() {
+	
+	private func validate() -> Bool {
+		// reset Error Message to empty string when button is tapped
+		errorMessage = ""
 		
+		guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+				!password.trimmingCharacters(in: .whitespaces).isEmpty else {
+			
+			errorMessage = "Please fill in all fields"
+			return false
+		}
+		
+		guard email.contains("@") && email.contains(".") else {
+			
+			errorMessage = "Please enter a valid email"
+			return false
+		}
+		return true
 	}
 }
